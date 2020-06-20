2fcee1458bf999a4006c17165d1e57d88c351bd8
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-20 16:31:47 -0300

    Fixing script

--- scripts/clearResource.sh
+++ scripts/clearResource.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+appId="04a11d74-feb5-4b5e-a34a-5b30cf2c436c"
+password="zm1zBYWHSREgJwYFH80H~IPV.3CGEahSyd"
 resourceGroupName="rs-gp-dev-001"
 kuberneteServiceName="aks-dev-001"
 
@@ -8,7 +10,7 @@ kuberneteServiceCheck=`az aks list --query "[?name=='$kuberneteServiceName']"`
 
 if [ ${#kuberneteServiceCheck} -gt 2 ]; then
     echo "$kuberneteServiceName: removing..."
-    az aks delete --name $resourceGroupName --yes
+    az aks delete --name $resourceGroupName --service-principal $appId --client-secret $password --yes
     echo "$kuberneteServiceName: removed successfully"
 else
 	echo "$kuberneteServiceName: not exists"
--- scripts/initializeResource.sh
+++ scripts/initializeResource.sh
@@ -7,6 +7,8 @@ resourceTags="AmberEggApi Dev KeepOnlineOnlyWorkTime"
 resourceGroupName=rs-gp-dev-001
 resourceGroupLocation=eastus
 resourceKubernetesName=aks-dev-001
+resourceGroupName="rs-gp-dev-001"
+kuberneteServiceName="aks-dev-001"
 #
 # Create 
 #
@@ -15,5 +17,9 @@ az group create --name $resourceGroupName --location eastus --tags $tags
 echo "Resource group created successfully"
 
 echo "Creating resource-kubernetes named $resourceKubernetesName with tags $resourceTags"
-az aks create --resource-group $resourceGroupName --name $resourceKubernetesName --node-count 1 --enable-addons monitoring --generate-ssh-keys --tags $tags
+az aks create --resource-group $resourceGroupName \
+	--name $resourceKubernetesName --node-count 1 \
+	--enable-addons monitoring --generate-ssh-keys \
+	--tags $tags \
+	--service-principal $appId --client-secret $password
 echo "Resource kubernetes created successfully"
