2a3c07e1682965baa5d03280b0a78228e3aafa3f
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-21 19:02:23 -0300

    fix script

--- scripts/createvnet.sh
+++ scripts/createvnet.sh
@@ -9,11 +9,11 @@ resourceSubnetName=$3
 resourceTags=$4
 
 echo "$resourceVnetName: check if exists"
-resourceVnetCheck=`az vnet list --query "[?name=='$resourceVnetName']"`
+resourceVnetCheck=`az network vnet list --query "[?name=='$resourceVnetName']"`
 
 if [ ${#resourceVnetCheck} -lt 3 ]; then
     echo "Creating vnet named $resourceVnetName with tags $resourceTags"
-	az network vnet create -g resourceGroupName -n $resourceVnetName --address-prefix 10.0.0.0/16 \
+	az network vnet create -g $resourceGroupName -n $resourceVnetName --address-prefix 10.0.0.0/16 \
 		--subnet-name $resourceSubnetName --subnet-prefix 10.240.0.0/16 \
 		--tags "$tags"
     echo "Vnet created successfully"
