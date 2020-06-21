56d580c290dfc563cb1c4efb13a5258c86c6a400
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-21 19:39:53 -0300

    fix script

--- scripts/createvnet.sh
+++ scripts/createvnet.sh
@@ -16,6 +16,19 @@ if [ ${#resourceVnetCheck} -lt 3 ]; then
 	az network vnet create -g $resourceGroupName -n $resourceVnetName --address-prefix 10.0.0.0/8 \
 		--subnet-name $resourceSubnetName --subnet-prefix 10.240.0.0/16 \
 		--tags $resourceTags
+		
+	az network vnet subnet create --address-prefixes 10.0.0.0/29 \
+        --name api-management-subnet \
+        --resource-group $resourceGroupName \
+        --vnet-name $resourceVnetName \
+		--tags $resourceTags
+	
+	az network vnet subnet create --address-prefixes 10.0.1.0/24 \
+        --name api-ingress-subnet \
+        --resource-group $resourceGroupName \
+        --vnet-name $resourceVnetName \
+		--tags $resourceTags
+		
     echo "Vnet created successfully"
 else
 	echo "$resourceVnetName: already exists"
