97e8fa9f6dc2049af4907919a551bd28d078db57
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-21 18:58:42 -0300

    fix pipeline

--- scripts/createvnet.sh
+++ scripts/createvnet.sh
@@ -8,15 +8,15 @@ resourceVnetName=$2
 resourceSubnetName=$3
 resourceTags=$4
 
-echo "$resourceGroupName: check if exists"
-resourceGroupCheck=`az group list --query "[?name=='$resourceGroupName']"`
+echo "$resourceVnetName: check if exists"
+resourceVnetCheck=`az vnet list --query "[?name=='$resourceVnetName']"`
 
-if [ ${#resourceGroupCheck} -lt 3 ]; then
-    echo "Creating resource-group named $resourceGroupName with tags $resourceTags"
+if [ ${#resourceVnetCheck} -lt 3 ]; then
+    echo "Creating vnet named $resourceVnetName with tags $resourceTags"
 	az network vnet create -g resourceGroupName -n $resourceVnetName --address-prefix 10.0.0.0/16 \
 		--subnet-name $resourceSubnetName --subnet-prefix 10.240.0.0/16 \
 		--tags "$tags"
-    echo "Resource group created successfully"
+    echo "Vnet created successfully"
 else
-	echo "$resourceGroupName: already exists"
+	echo "$resourceVnetName: already exists"
 fi
