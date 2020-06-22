c47e79147fc0fd5c9557dfe4845374a648bc8dae
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-21 21:23:34 -0300

    Create PowerShell apim create

---
+++ scripts/createapim.ps1
@@ -0,0 +1,20 @@
+$subscription = args[0]
+$resourceGroupName = args[1]
+$resourceVnetName = args[2]
+$resourceSubnetName = args[3]
+$apiManagementName = args[4]
+$apiManagementPublisherEmail = args[5]
+$apiManagementPublisherName = args[6]
+$resourceTags = args[7]
+
+write-host "$subscription"
+write-host "$resourceGroupName"
+write-host "$resourceVnetName"
+write-host "$resourceSubnetName"
+write-host "$apiManagementName"
+write-host "$apiManagementPublisherEmail"
+write-host "$apiManagementPublisherName"
+write-host "$resourceTags"
+
+$virtualNetwork = New-AzApiManagementVirtualNetwork -Location "East US" -SubnetResourceId "/subscriptions/$subscription/resourceGroups/$resourceGroupName/providers/Microsoft.Network/virtualNetworks/$resourceVnetName/subnets/api-management-subnet"
+New-AzApiManagement -ResourceGroupName $resourceGroupName -Location "East US" -Name "$apiManagementName" -Organization "$apiManagementName" -AdminEmail "$apiManagementPublisherEmail" -Sku "Developer" -VpnType "External" -Tags "$resourceTags" -VirtualNetwork $virtualNetwork
