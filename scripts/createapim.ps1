1ba436e85607b74a812b2f23f5b7d54b9fe00442
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-22 06:46:12 -0300

    Fix script

--- scripts/createapim.ps1
+++ scripts/createapim.ps1
@@ -14,14 +14,15 @@ $apiManagementPublisherEmail = $args[5]
 $apiManagementPublisherName = $args[6]
 $resourceTags = ArrayToHash($args[7].Split(" "))
 
-$apiManagementCheck = Get-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apiManagementName
 
-if($apiManagementCheck.Length -lt 1) {
-    write-host "Creating api management $apiManagementName"
+try {
+	Get-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apiManagementName
+	write-host "$apiManagementName already exists"
+}
+Catch {
+	write-host "Creating api management $apiManagementName"
     $virtualNetwork = New-AzApiManagementVirtualNetwork -SubnetResourceId "/subscriptions/$subscription/resourceGroups/$resourceGroupName/providers/Microsoft.Network/virtualNetworks/$resourceVnetName/subnets/api-management-subnet"
     New-AzApiManagement -ResourceGroupName $resourceGroupName -Location "East US" -Name $apiManagementName -Organization $apiManagementName -AdminEmail $apiManagementPublisherEmail -Sku "Developer" -VpnType "External" -VirtualNetwork $virtualNetwork
-}else {
-	write-host "$apiManagementName already exists"
 }
 
 
